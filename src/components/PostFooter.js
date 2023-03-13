f3beabebdf84e5055b18f2b94aae4f9a35918c3c
tiagomiotto <tiago.miotto@hotmail.com>
2023-03-13 21:57:55 +0000

    Add PostFooter and PostFooter.css

--- src/components/PostFooter.js
+++ src/components/PostFooter.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "./PostFooter.css";
 
-export function PostFooter(props) {
-  return <div></div>;
+export function PostFooter({ author_fullname, created, num_comments }) {
+  return (
+    <div className="post-footer">
+      <div className="post-footer-author post-footer-item">
+        By{" "}
+        <a href={`https://reddit.com/u/${author_fullname}`}>
+          {author_fullname}
+        </a>
+      </div>
+      <div className="post-footer-created  post-footer-item">At {created}</div>
+      <div className="post-footer-comments  post-footer-item">
+        {num_comments} comments
+      </div>
+    </div>
+  );
 }
