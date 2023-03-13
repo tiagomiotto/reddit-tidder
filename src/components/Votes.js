801ba9535312e98798371df7758a215f11cd2120
tiagomiotto <tiago.miotto@hotmail.com>
2023-03-13 23:57:30 +0000

    Initial Vote component upload

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,5 +1,18 @@
 import React from "react";
+import downVoteIcon from "../assets/downvote.svg";
+import upVoteIcon from "../assets/upvote.svg";
+import "./Votes.css";
 
-export function Votes(props) {
-  return <div></div>;
+export function Votes({ score }) {
+  return (
+    <div className="post-vote-buttons">
+      <img src={upVoteIcon} className="upvote-icon" alt="Upvote button"></img>
+      <div>{score}</div>
+      <img
+        src={downVoteIcon}
+        className="downvote-icon"
+        alt="Downvote icon"
+      ></img>
+    </div>
+  );
 }
--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Votes } from "../../components/Votes";
 import { PostFooter } from "../../components/PostFooter";
+import "./Post.css";
 
 export function Post({
   id,
@@ -16,13 +17,18 @@ export function Post({
       <Votes score={score} />
       <div className="post-content-frame">
         <h2 className="post-title">{title}</h2>
-        <img src="" alt="" className="post-image-preview"></img>
+        <img
+          src="https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_960_720.png"
+          alt=""
+          className="post-image-preview"
+        ></img>
+
+        <PostFooter
+          author_fullname={author_fullname}
+          num_comments={num_comments}
+          created={created}
+        />
       </div>
-      <PostFooter
-        author_fullname={author_fullname}
-        num_comments={num_comments}
-        created={created}
-      />
     </div>
   );
 }
