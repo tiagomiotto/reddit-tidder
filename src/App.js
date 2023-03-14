1c8a002b79659dc8b1d8b02b7468f103254ce4c1
tiagomiotto <tiago.miotto@hotmail.com>
2023-03-14 11:55:41 +0000

    Remove unused variables from App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import { PostList } from "./features/posts/PostList";
 import "./App.css";
-import { Post } from "./features/posts/Post";
 
 function App() {
   return (
