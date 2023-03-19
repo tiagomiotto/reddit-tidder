c6e59bb114f1af9617969e11e034db7f92c9d16f
tiagomiotto <tiago.miotto@hotmail.com>
2023-03-19 19:16:11 +0000

    Add search box to the Post List

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { PostList } from "./features/posts/PostList";
 import "./App.css";
 import { Header } from "./components/Header/Header";
+import { SearchBar } from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,25 +2,27 @@ import React from "react";
 import "./SearchBar.css";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { setSearchTerm } from "../../features/posts/postsSlice";
 
 export function SearchBar() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [search, setSearch] = useState("");
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
-    setSearchTerm(e.target.value);
+    setSearch(e.target.value);
+    dispatch(setSearchTerm(e.target.value));
   };
 
   return (
     <div className="search-bar">
       <input
-        type="text"
+        type="search"
         name="search"
         placeholder="Search..."
-        value={searchTerm}
+        value={search}
         onChange={handleChange}
       />
-      <i class="fa-solid fa-magnifying-glass"></i>
+      {/* <i className="fa-solid fa-magnifying-glass"></i> */}
     </div>
   );
 }
