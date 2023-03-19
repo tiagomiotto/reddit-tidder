86c02ffa035d04eb6bf3a54435a9be20181b18ec
tiagomiotto <tiago.miotto@hotmail.com>
2023-03-19 19:16:52 +0000

    Remove unused vars and finalize Search Feature

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import { PostList } from "./features/posts/PostList";
 import "./App.css";
 import { Header } from "./components/Header/Header";
-import { SearchBar } from "./components/SearchBar/SearchBar";
-
 function App() {
   return (
     <div className="App">
