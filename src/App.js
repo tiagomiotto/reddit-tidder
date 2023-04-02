c2b2884b5fefaefe459088c8e42b24e73aca05f9
Tiago Miotto <tiago.miotto@hotmail.com>
2023-04-02 14:59:57 +0100

    Import React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React from "react";
 import { PostList } from "./features/posts/PostList";
 import "./App.css";
 import { Header } from "./components/Header/Header";
+import { Route } from "react-router-dom";
+import { PostDetail } from "./features/posts/PostDetail/PostDetail";
 function App() {
   return (
     <div className="App">
       <header>
         <Header />
       </header>
-      <PostList />
+      <Route path="/" element={
+        <PostList />} />
+      <Route path="/posts/:id" element={
+        <PostDetail />} />
     </div>
   );
 }
